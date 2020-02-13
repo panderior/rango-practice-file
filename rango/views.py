91ae0d83409179529301a0eca13829fe478a3533
Duguma Y. H/M <dugyeshi@gmail.com>
2020-02-13 14:46:43 +0300

    added image to index.html and configured template and static dirs

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    idx = "<h2>Rango says hey there partner!</h2><br><a href='/rango/about'>About</a>"
-    return HttpResponse(idx)
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
     abt = "<h2>Rango says here is the about page</h2><br><a href='/rango/'>Home</a>"
     return HttpResponse(abt)
+
