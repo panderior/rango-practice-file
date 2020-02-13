247f8f8eeca2f423d78f81ba6bd70c64feb74b6a
Duguma Y. H/M <dugyeshi@gmail.com>
2020-02-13 03:30:38 +0300

    Added all files for first full commit

---
+++ tango_with_django_project/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+from rango import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    # any url pattern starting with 'rango' is directed to the
+    # rango urls.py file
+    url(r'^xit'
+        r'rango/', include('rango.urls')),
+]
