b4eaee5b43056bbeabda946384fcc119a67ba9fd
Duguma Y. H/M <dugyeshi@gmail.com>
2020-04-05 08:10:25 +0300

    Tenth chapter done - handled cookies and sessions

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,35 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+# HELPER FUNCTIONS
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(
+        request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(
+        last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# VIEWS
 
 
 def index(request):
@@ -12,19 +41,34 @@ def index(request):
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     # context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 
+    # request.session.set_test_cookie()     # set test cookie
+
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
 
-    return render(request, 'rango/index.html', context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/index.html', context_dict)
+    return response
 
 
 def about(request):
     # prints out whether the method is a GET or a POST
+
+    # if (request.session.test_cookie_worked()):    # check test cookie
+    # print("TEST COOKIE WORKED!")
+    # request.session.delete_test_cookie()
+
     print(request.method)
     # prints out the user name, if no one is logged in it prints `AnonymousUser`
     print(request.user)
-    return render(request, 'rango/about.html', {})
+
+    visitor_cookie_handler(request)
+    context_dict = {'visits': request.session['visits']}
+    response = render(request, 'rango/about.html', context_dict)
+    return response
 
 
 def show_category(request, category_name_slug):
